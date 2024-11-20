545da911f3876bf2c6bd2bca5a73e2cc2a7958cf
Kamila Pawluk <pawlukkamila08@gmail.com>
2024-11-20 14:42:21 +0100

    backupAfterClasses

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
 let counter = 0;
-let counterObject = 0;
+const elementCounters = {};
+let arrayInventory = [];
+const counterObject = 0;
 const quantity = document.getElementById('containerWithQuantity');
+const box = document.getElementById("inventoryList");
 
 function addObject(element){
-    hashcode
-    let box = document.getElementById("inventoryList");
-    if(!box.contains(element)) {
-        box.innerHTML += element.outerHTML;
+    const elementId = element.elementId; 
+    if(!arrayInventory.includes(elementId)){
+        arrayInventory.map(element, counterObject);
+        counterObject++;
     } else {
-        box.innerHTML += counter; 
+       arrayInventory.push(element);
+        console.log(arrayInventory);
     }
-    counter++;
-   }
-
+}
+// function addObject11(element){
+// const elementId = element.elementId;  
+//   if(elementCounters[elementId]){
+//     const existingElement = box.querySelector(elementId);
+//     existingElement.innerHTML += `${counterObject++}`; 
+//   } else {
+//     const newInInventory = element.outerHTML;
+//     box.innerHTML += newInInventory;  
+//     elementCounters[elementId]++;
+//   }
+// }
+ 
    
 var object1 = 0;
 var object2 = 0;
 var object3 = 0;
 var object4 = 0;
 var object5 = 0;
-var bagtest = 1;
+var bagstate = 1;
 
 function bag(){
-    if (bagtest == 1) {
-    bagtest--;
+    if (bagstate == 1) {
+    bagstate--;
     document.getElementById('inventoryContainer').style.opacity = '0%';
     document.getElementById('containerWithQuantity').classList.remove('quantity');
     document.getElementById('quantityInInventory').innerHTML = "Antall i inventory: " + counter;
@@ -31,42 +45,33 @@ function bag(){
     else {
     document.getElementById('inventoryContainer').style.opacity = '100%';
     document.getElementById('containerWithQuantity').classList.add('quantity');
-    bagtest++;
+    bagstate++;
     }
-}
-
-function hideInventory() {
-    // var inventoryState = hidden;
-    document.getElementById('inventoryContainer').style.opacity = '0%';
-    document.getElementById('containerWithQuantity').classList.remove('quantity');
-    document.getElementById('quantityInInventory').innerHTML = "Antall i inventory: " + counter;
-    // updateView();
-}
-function showInventory() {
-    document.getElementById('inventoryContainer').style.opacity = '100%';
-    document.getElementById('containerWithQuantity').classList.add('quantity');
-
 }
 function addObject1() {
     object1++;
-    document.getElementById('testtest').innerHTML = (`object1 = ${object1}`)
+    updateView();
 }
 function removeObject1() {
-    if (object1 === 1) {object1--;
-    document.getElementById('testtest').innerHTML = ("")
+    if (object1 === 0) {
     }
-    else if (object1 === 0) {
+    else if (object1 === 1) {
+    object1--;
+    document.getElementById('item1div').remove();
     }
     else {
     object1--;
-    document.getElementById('testtest').innerHTML = (`object1 = ${object1}`)
+    updateView();
     }
 }
-
+const returnId = document.getElementById("mainInventoryDiv");
 function updateView() {
-    // kan ha if funksjon, if object 1 =>0 load immage og vis counter til object
-// quantity.innerHTML = /*HTML*/ `
-// ${addObject()}
-// `
-
+    returnId.innerHTML = /*HTML*/ `
+     <div onclick="removeObject1()" id="item1div">
+      <img id="inventoryImgs" src="https://wow.zamimg.com/images/wow/icons/large/inv_misc_bag_08.jpg">
+      <span> name </span>
+      <span>${object1}</span>
+     </div>
+     <br>
+    `;
 }
